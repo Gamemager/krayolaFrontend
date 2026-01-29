7be2f051ed71b87b790b6a5ca196cdc286bfb0fa
Eixon <eixond@gmail.com>
2026-01-28 21:25:32 -0500

    bugHamburgerMenu

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -5,11 +5,28 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [RouterLink, CommonModule],
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrl: './header.css', // Asegúrate que sea styleUrl (Angular 17+) o styleUrls
 })
 export class Header {
-  public cartService = inject(CartService)
-  
+  public cartService = inject(CartService);
+
+  // Variable de estado para controlar el menú
+  isMenuOpen = false;
+
+  /**
+   * Alterna el estado (para el botón hamburguesa)
+   */
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  /**
+   * Cierra el menú forzosamente (para los enlaces)
+   */
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
